06551bb28026d23778385108b023f5d55105bda6
zhengdou <zhengdou1992@live.com>
2017-06-21 22:26:31 +0800

    添加文章及评论api

--- view.py
+++ view.py
@@ -1,12 +1,12 @@
 # coding = utf-8
-from flask import Flask, render_template, url_for, jsonify, request
+from flask import Flask, render_template, url_for, jsonify, request, redirect
 from flask_restful import reqparse, Api, Resource
 from model import *
 
 
 app = Flask(__name__)
 api = Api(app)
-app.secret_key = 'liangchuannan'
+app.secret_key = 'temp secret key'
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
@@ -26,15 +26,43 @@ def blog():
     return render_template('blog.html', pagination=pagination, categorys=categorys)
 
 
-@app.route('/article/<int:id>')
-def article(id):
-	article = articles[id-1]
+@app.route('/article/<name>')
+def article(name):
+	article = Article.query.get_or_404(title=name)
 	return render_template('article.html', article=article)
+
+
+class Article(Resource):
+	def get(self, name):
+		article = Article.query.get_or_404(title=name)
+		return {'article': article}
+
+	def put(self, name):
+		params = ['title', 'category', 'tags', 'md_content']
+		parser = reqparse.RequestParser()
+		for param in params:
+			parser.add_argument(param, type = str, required = True,help = 'No '+param+' provided')
+		article = Article()
+		args = parser.parse_args()
+		for key, value in args.items():
+			article[key] = value
+		try:
+			db.session.add(article)
+			db.session.commit()
+			return redirect('/article/name')
+		except Exception, e:
+			return {'message': e}, 400
+
 
 
 class CommentList(Resource):
 	def get(self):
-		return comments
+		article_id = request.args.get('article_id', None)
+		if article_id:
+			comments = Comment.query.filter_by(article_id=article_id).all()
+			return {'comments': comments}
+		else:
+			return {'message': 'article id is wrong'}, 400
 
 	def post(self):
 		parser = reqparse.RequestParser()
@@ -47,7 +75,8 @@ class CommentList(Resource):
 		return {'id': args['id']}
 
 
-api.add_resource(CommentList, '/comments')
+api.add_resource(CommentList, '/api/comments')
+api.add_resource(Article, '/api/article/<name>')
 
 
 
