f4b80f8d2ba70afb8010876086dbcb4d376d98cf
zhengdou <zhengdou1992@live.com>
2017-06-22 21:52:09 +0800

    增加评论及评论回复功能

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from datetime import datetime
+from datetime import datetime, timedelta
 import re
 from flask_sqlalchemy import SQLAlchemy
 import mistune
@@ -46,7 +46,7 @@ class Article(db.Model):
     summary = db.Column(db.Text)
     md_content = db.Column(db.Text)
     html_content = db.Column(db.Text)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.DateTime, default=(datetime.utcnow()+timedelta(hours=8)))
     read_amount = db.Column(db.Integer, default=0)
     
     @staticmethod
@@ -87,7 +87,7 @@ class Comment(db.Model):
     reply_user = db.Column(db.String(128), nullable=True)
     content = db.Column(db.Text)
     hide = db.Column(db.Integer, default=0)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.DateTime, default=(datetime.utcnow()+timedelta(hours=8)))
 
     def __repr__(self):
-        return '<Comment %r>'%self.name
+        return '<Comment %r>'%self.user
--- view.py
+++ view.py
@@ -1,5 +1,5 @@
 # coding = utf-8
-from flask import Flask, render_template, url_for, jsonify, request, redirect
+from flask import Flask, render_template, url_for, jsonify, request, redirect, abort
 from flask_restful import reqparse, Api, Resource
 from model import *
 
@@ -28,16 +28,21 @@ def blog():
 
 @app.route('/article/<name>')
 def article(name):
-	article = Article.query.get_or_404(title=name)
+	article = Article.query.filter_by(title=name).first()
+	if not article:
+		return abort(404)
 	return render_template('article.html', article=article)
 
 
-class Article(Resource):
-	def get(self, name):
-		article = Article.query.get_or_404(title=name)
-		return {'article': article}
+class Post(Resource):
+	def get(self):
+		id = request.args.get('id', None)
+		if not id:
+			return {'message': 'no id'}, 400
+		article = Article.query.get_or_404(id)
+		return article
 
-	def put(self, name):
+	def put(self):
 		params = ['title', 'category', 'tags', 'md_content']
 		parser = reqparse.RequestParser()
 		for param in params:
@@ -60,23 +65,30 @@ class CommentList(Resource):
 		article_id = request.args.get('article_id', None)
 		if article_id:
 			comments = Comment.query.filter_by(article_id=article_id).all()
-			return {'comments': comments}
+			resp = []
+			for comment in comments:
+				resp.append({'id':comment.id, 'reply_user':comment.reply_user, 'user':comment.user, 'date':comment.date.strftime("%Y-%m-%d %H:%M:%S"), 'content':comment.content})
+			return resp
 		else:
 			return {'message': 'article id is wrong'}, 400
 
 	def post(self):
+		article_id = request.args.get('article_id', None)
 		parser = reqparse.RequestParser()
-		parser.add_argument('user', type = str, required = True,help = 'No user provided')
-		parser.add_argument('date', type = str, required = True,help = 'No user provided')
-		parser.add_argument('content', type = str, required = True,help = 'No user provided')
+		parser.add_argument('user', required = True,help = 'No user provided')
+		parser.add_argument('reply_user', required = True,help = 'No user provided')
+		parser.add_argument('content', required = True,help = 'No user provided')
 		args = parser.parse_args()
-		args['id'] = comments[-1]['id'] + 1
-		comments.append(args)
-		return {'id': args['id']}
+		comment = Comment(article_id=article_id, reply_user=args['reply_user'], user=args['user'], content=args['content'])
+		db.session.add(comment)
+		db.session.commit()
+		new_comment = Comment.query.filter_by(article_id = article_id, user = args['user']).\
+			order_by(Comment.date.desc()).first()
+		return {'id':new_comment.id, 'date':new_comment.date.strftime("%Y-%m-%d %H:%M:%S")}
 
 
 api.add_resource(CommentList, '/api/comments')
-api.add_resource(Article, '/api/article/<name>')
+api.add_resource(Post, '/api/article')
 
 
 
